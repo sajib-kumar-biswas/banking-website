f6f96aeebe0ee1ab100fa6967318fe072e24af46
sajib_kumar_biswas <sajibbiswas01521457841@gmail.com>
2022-11-26 12:40:59 +0600

    done with the basic functionalities..

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,53 +1,54 @@
-"use strict";
 // things we need to update
 // const deposit = document.getElementById("deposit");
 // const withdraw = document.getElementById("withdraw");
 // const balance = document.getElementById("balance");
+
 // buttons 
 const depositBtn = document.getElementById("depositBtn");
 const withdrawBtn = document.getElementById("withdrawBtn");
+
 // input fields
-const inputDeposit = document.getElementById("inputDeposit");
-const inputWithdraw = document.getElementById("inputWithdraw");
+const inputDeposit = document.getElementById("inputDeposit") as HTMLInputElement | null;
+const inputWithdraw = document.getElementById("inputWithdraw") as HTMLInputElement | null;
+
 // getter function
-function getCurrentValue(id) {
+function getCurrentValue(id: string): number {
     const amountElement = document.getElementById(id);
-    const amountText = amountElement ? amountElement.innerText : "0";
+    const amountText:string = amountElement? amountElement.innerText : "0";
     return parseFloat(amountText);
 }
+
 // updater function
-function update(amount, id, isAdd) {
+function update(amount: number, id: string, isAdd: boolean){
     const current = getCurrentValue(id);
     const element = document.getElementById(id);
-    if (element) {
-        if (isAdd) {
+    if(element) {
+        if(isAdd) {
             element.innerText = String(current + amount);
-        }
-        else {
+        } else {
             element.innerText = String(current - amount);
         }
-    }
+    } 
 }
+
 // function to help
-function helper(element, isDeposit) {
+function helper(element: any, isDeposit: boolean) {
     const amount = parseFloat(element.value);
     element.value = "";
     const balance = getCurrentValue("balance");
-    if (isDeposit) {
+    if(isDeposit) {
         update(amount, "deposit", true);
         update(amount, "balance", true);
-    }
-    else if (amount >= 1 && Math.floor(balance) > amount) {
+    } else if(amount >= 1 && Math.floor(balance) > amount) {
         update(amount, "withdraw", true);
-        update(amount, "balance", false);
-    }
-    else if (amount <= 0) {
+        update(amount, "balance", false)
+    } else if(amount <=0 ) {
         alert("Invalid amount!!");
-    }
-    else {
+    } else {
         alert("Insufficient balance!!");
     }
 }
+
 // adding event listeners for the buttons
-depositBtn === null || depositBtn === void 0 ? void 0 : depositBtn.addEventListener("click", () => helper(inputDeposit, true));
-withdrawBtn === null || withdrawBtn === void 0 ? void 0 : withdrawBtn.addEventListener("click", () => helper(inputWithdraw, false));
+depositBtn?.addEventListener("click", () => helper(inputDeposit, true));
+withdrawBtn?.addEventListener("click", () => helper(inputWithdraw, false));
